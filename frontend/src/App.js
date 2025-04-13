35d61f0e79cfe3673e1711ca312d7d5098ee240c
Vagdevi Sagi <sagivagdevi@gmail.com>
2025-04-13 21:48:25 +0530

    save current files

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [msg, setMsg] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api')
+      .then(res => res.json())
+      .then(data => setMsg(data.message))
+      .catch(err => setMsg('Error: Could not fetch message'));
+  }, []);
+
+  return (
+    <div>
+      <h1>React + Express + WAF</h1>
+      <p>{msg}</p>
+    </div>
+  );
+}
+
+export default App;
